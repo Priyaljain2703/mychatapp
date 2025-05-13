b0c7b687e191ef34fa20d4efd09375790e2a7012
Priyaljain2703 <priyaljain2703@gmail.com>
2025-05-13 19:34:46 +0530

    chnages done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
     return (
     <div className="relative justify-between items-center p-4 border-[#dbd9d9] border-r-2 w-[5vw] ">
       <button onClick={handleChat} className='hover:bg-[#efefef] p-1.5 rounded-full focus:bg-[#e8f8ff] focus:outline-none focus:ring-2 focus:ring-[#e2e2e2]  '>
-        <Image src="/Group.svg" alt="Logo" width={6} height={6}  />
+        <Image src="/Group.svg" alt="Logo" width={6} height={6} className="h-6 w-6" />
       </button>
       <div className=" flex flex-col space-x-4  my-4">
 
@@ -29,7 +29,7 @@ function Navbar() {
         className="mx-2"
         onClick={handleLogout}
         >
-          <Image src='/logout.svg' width={6} height={6} />
+          <Image src='/logout.svg' className='w-10 h-10' width={6} height={6} />
         </button>
       </div>
     </div>
